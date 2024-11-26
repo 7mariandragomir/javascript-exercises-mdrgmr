5b6c9e24f014f7b293ba5c2a58cb82022af88680
Marian <7mariandragomir@gmail.com>
2024-11-26 22:43:22 +0200

    solved ex 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,80 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (!Array.isArray(arr)) {
+    console.log("Please use an array, [a, b, c]");
+  }
+
+  let result; 
+
+  result = arr.reduce((previous, next) =>{
+    return previous + next;
+  }, 0);
+
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  if (!Array.isArray(arr)) {
+    console.log("Please use an array, [a, b, c]");
+  }
+  let result;
 
+  result = arr.reduce((previous, next) =>{
+    return previous * next;
+  })
+
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) { //I should have used Math.pow
+  let arr = [];
+  let result; 
+
+  for(let i = 0; i < exponent; i++) {
+    arr.push(base);
+    console.log(arr);
+  };
+
+  result = arr.reduce((prev, next) =>{
+    return prev * next;
+  });
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(f) {
+	let result; 
+  let arr = []; 
+  let num = f;
+
+  for (let i = 0; i < f; i++) {
+    arr.push(num);
+    num = num - 1;
+  }
+
+  result = arr.reduce((prev, next) => {
+    return prev * next;
+  }, 1);
+
+  return result;
 };
+
+// const factorial = function (n) {
+//   if (n === 0) return 1;
+//   let product = 1;
+//   for (let i = n; i > 0; i--) {
+//     product *= i;
+//   }
+//   return product;
+// };
+
 
 // Do not edit below this line
 module.exports = {
