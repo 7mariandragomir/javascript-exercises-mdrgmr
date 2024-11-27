d673dcdeddebba7abd89f06f2f18435924a8668a
Marian <7mariandragomir@gmail.com>
2024-11-27 18:20:40 +0200

    solved ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let fibSeq = [1, 1];
+    let num = Number(n);
 
+    if(Math.sign(num) === -1) {return "OOPS"};
+    if(num === 0) {return 0}
+
+    
+
+    for(let i = 2; i < num; i++) {
+        item = fibSeq[fibSeq.length-1] + fibSeq[fibSeq.length-2]
+        fibSeq.push(item);
+    }
+    
+    console.log(fibSeq[fibSeq.length-1]);
+    return fibSeq[fibSeq.length-1];
 };
+
+fibonacci(0);
 
 // Do not edit below this line
 module.exports = fibonacci;
