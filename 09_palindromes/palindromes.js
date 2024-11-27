4992e995f5fe443c4765bdcabcd049a1970893a5
Marian <7mariandragomir@gmail.com>
2024-11-27 17:50:13 +0200

    solved ex 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let arr = string.replace(/[^a-zA-Z0-9]/g, '').split('');
+    let reverseArr = arr.toReversed(); 
 
+    if(arr.join('').toLowerCase() === reverseArr.join('').toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
-
 // Do not edit below this line
 module.exports = palindromes;
